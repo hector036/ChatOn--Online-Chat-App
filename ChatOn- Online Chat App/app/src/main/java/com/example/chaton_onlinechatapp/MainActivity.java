df9856280c8beedfab0b420dd79ff86b9cfc9bd0
hector036 <nayeem1505>
2019-11-28 01:20:04 +0600

    first commit

---
+++ ChatOn- Online Chat App/app/src/main/java/com/example/chaton_onlinechatapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.chaton_onlinechatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccesserAdapter myTabsAccesserAdapter;
+
+    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mToolbar =  findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("ChatOn");
+
+
+        myViewPager = findViewById(R.id.main_tabs_pager);
+        myTabsAccesserAdapter = new TabsAccesserAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccesserAdapter);
+
+
+        myTabLayout = findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        
+        if(currentUser == null){
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+
+        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
+    }
+}
